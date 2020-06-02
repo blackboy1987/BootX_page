596ce2a3fc39a8896b97292009d82b886d8c3724
1169794338@qq.com <1169794338@qq.com>
2020-06-02 15:27:51 +0800

    1.岗位管理增加部门关联

--- src/pages/index/release/index.ts
+++ src/pages/index/release/index.ts
@@ -1,6 +1,7 @@
 import log_20200504 from './20200504';
 import log_20200505 from './20200506';
+import log_20200602 from './20200602';
 
-export const ReleaseLog = [log_20200505, log_20200504];
+export const ReleaseLog = [log_20200602, log_20200505, log_20200504];
 
 export default ReleaseLog;
