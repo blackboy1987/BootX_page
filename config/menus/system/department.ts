bad6ad4c8498233b360b5e20f31736dee58a5aab
1169794338@qq.com <1169794338@qq.com>
2020-06-02 15:19:18 +0800

    1.岗位管理增加部门关联

--- config/menus/system/department.ts
+++ config/menus/system/department.ts
@@ -1,10 +1,5 @@
 export default {
   name: 'department',
   path: '/system/department',
-  routes: [
-    {
-      path: '/system/department',
-      component: './system/department',
-    },
-  ],
+  component: './system/department',
 };
