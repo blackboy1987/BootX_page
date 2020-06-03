7b4f4ebd050f1d34af653cc44f9001d6d79e7375
1169794338@qq.com <1169794338@qq.com>
2020-06-03 10:30:02 +0800

    1.岗位管理增加部门关联

--- src/pages/system/word/data.d.ts
+++ src/pages/system/word/data.d.ts
@@ -1,41 +1,30 @@
 /**
  * 列表展示的字段
  */
+import {
+  CommonTableListData,
+  CommonTableListPagination,
+  CommonTableListParams,
+} from '@/utils/interface/common';
+
 export interface TableListItem {
-  id: number;
-  name: string;
-  memo: string;
-  isEnabled: boolean;
-  isDefault: boolean;
-  departmentName: string;
-  createdDate: Date;
+  id?: number;
+  name?: string;
+  memo?: string;
+  isEnabled?: boolean;
+  isDefault?: boolean;
+  departmentName?: string;
+  createdDate?: Date;
 }
 
 /**
  * 分页数据
  */
-export interface TableListPagination {
-  total: number;
-  pageSize: number;
-  current: number;
-}
+export interface TableListPagination extends CommonTableListPagination {}
 
-export interface TableListData {
-  list: TableListItem[];
-  pagination: Partial<TableListPagination>;
-}
+export interface TableListData extends CommonTableListData {}
 
 /**
  * 搜索的数据
  */
-export interface TableListParams {
-  id: null;
-  sorter: string;
-  status: string;
-  name: string;
-  current: number;
-  currentPage: number;
-  pageNumber: number;
-  orderProperty: string;
-  orderDirection: string;
-}
+export interface TableListParams extends CommonTableListParams {}
