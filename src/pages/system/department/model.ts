01adf2fa564e2ca9ee1bea8be422589b7f111b66
1169794338@qq.com <1169794338@qq.com>
2020-06-03 11:00:47 +0800

    1.岗位管理增加部门关联

--- src/pages/system/department/model.ts
+++ src/pages/system/department/model.ts
@@ -1,5 +1,4 @@
-import { AnyAction, Reducer } from 'umi';
-import { EffectsCommandMap } from 'dva';
+import { Effect, Reducer } from 'umi';
 import { list, save, tree, edit, remove } from './service';
 
 import { TableListItem } from './data.d';
@@ -8,11 +7,6 @@ export interface StateType {
   data: TableListItem[];
   value?: TableListItem;
 }
-
-export type Effect = (
-  action: AnyAction,
-  effects: EffectsCommandMap & { select: <T>(func: (state: StateType) => T) => T },
-) => void;
 
 export interface ModelType {
   namespace: string;
